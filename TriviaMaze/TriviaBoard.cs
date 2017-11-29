4bb471c56033c76bcef4cb73f4b062cb22217ae7
Adam Holzer <shortyforcs@gmail.com>
2017-11-29 10:14:23 -0800

    flushed out model objects more. implemented locks. need questions/questionform now

--- TriviaMaze/TriviaBoard.cs
+++ TriviaMaze/TriviaBoard.cs
@@ -13,16 +13,24 @@ namespace TriviaMaze
      */
     public class TriviaBoard
     {
+        /* only allocate questions in the rooms we've explored too.
+         * this infers that if the user has attempted every question on the board and failed
+         * they lose. first room only has 2 questions
+         */
         TriviaTile[,] GameMap;
         TriviaTile MyLoc;
         public int XPos { get; private set; }
         public int YPos{ get; private set; } 
         private readonly int MAX_SIZE;
+        public int GatesAvailable { get; private set; }
 
         public TriviaBoard()
         {
             GameMap = new TriviaTile[5, 5];
-            for(int i = 0; i < GameMap.GetLength(0); i++)
+
+            /* instead of instantiating all of them, we are only going to 
+             * instantiate rooms we've reached
+            for (int i = 0; i < GameMap.GetLength(0); i++)
             {
                 for(int j = 0; j < GameMap.GetLength(0); j++)
                 {
@@ -30,10 +38,29 @@ namespace TriviaMaze
 
                 }
                 
-            }
-            MyLoc = new TriviaTile();
+            }*/
+            BuildNextTile(XPos, YPos);
+            MyLoc = GameMap[XPos, YPos];
             MAX_SIZE = 5;
             XPos = YPos = 0;
+        }
+
+        //call to build a tile at the location passed in
+        private void BuildNextTile(int x, int y)
+        {
+            if (GameMap[x, y] == null)
+            {
+                Log($"Creating tile <{x},{y}> and assigning to MyLoc");
+                GameMap[x, y] = new TriviaTile(x, y);
+                MyLoc = GameMap[x, y];
+            }
+            else
+            {
+                Log("Tile already exists, Updating MyLoc");
+                MyLoc = GameMap[x, y];
+            }
+                
+
         }
 
         //Call to move up one spot
@@ -42,13 +69,16 @@ namespace TriviaMaze
             bool rv = true;
             YPos--;
             //check if invalid. if so, revert work done
-            if(YPos < 0)
+            if(YPos < 0 || MyLoc.NorthLock == Lock.HardLock)
             {
-                Log("Can't move up out of bounds");
+                Log("Can't move up out of bounds or through a hard lock");
                 YPos++;
                 return false;
             }
             //valid move, do work
+
+            //before i build the next tile, i need to check locks and ask question
+            BuildNextTile(XPos, YPos);
             return rv;
         }
 
@@ -57,37 +87,42 @@ namespace TriviaMaze
         {
             bool rv = true;
             YPos++;
-            if (YPos >= MAX_SIZE)
+            if (YPos >= MAX_SIZE || MyLoc.SouthLock == Lock.HardLock)
             {
-                Log("Can't move down out of bounds");
+                Log("Can't move down out of bounds or through a hard lock");
                 YPos--;
                 return false;
             }
             //valid move, do work
+            
+            //before i build the next tile, i need to check locks and ask question
+            BuildNextTile(XPos, YPos);
             return rv;
         }
         public bool MoveLeft()
         {
             bool rv = true;
             XPos--;
-            if (XPos < 0)
+            if (XPos < 0 || MyLoc.WestLock == Lock.HardLock)
             {
-                Log("Can't move left out of bounds");
+                Log("Can't move left out of bounds or through a hard lock");
                 XPos++;
                 return false;
             }
+            BuildNextTile(XPos, YPos);
             return rv;
         }
         public bool MoveRight()
         {
             bool rv = true;
             XPos++;
-            if (XPos >= MAX_SIZE)
+            if (XPos >= MAX_SIZE || MyLoc.EastLock == Lock.HardLock)
             {
-                Log("Can't move down out of bounds");
+                Log("Can't move down out of bounds or through a hard lock");
                 XPos--;
                 return false;
             }
+            BuildNextTile(XPos, YPos);
             return rv;
         }
         
--- TriviaMaze/TriviaTile.cs
+++ TriviaMaze/TriviaTile.cs
@@ -12,6 +12,66 @@ namespace TriviaMaze
     */
     public class TriviaTile
     {
+        public int XCoord { get; private set; }
+        public int YCoord { get; private set; }
+        public Lock NorthLock { get; private set; }
+        public Lock SouthLock { get; private set; }
+        public Lock EastLock { get; private set; }
+        public Lock WestLock { get; private set; }
 
+
+        public TriviaTile(int x, int y)
+        {
+            XCoord = x;
+            YCoord = y;
+            switch(x)
+            {
+                /* If x = 0, we are near the west boundary and need to hardlock it.
+                 * If x = max, we are near the east boundary and need to hardlock it.
+                 * otherwise, we are in the middle and both need to be softlocked.
+                 */
+                case 0:
+                    EastLock = Lock.SoftLock;
+                    WestLock = Lock.HardLock;
+                    break;
+
+                case 4:
+                    EastLock = Lock.HardLock;
+                    WestLock = Lock.SoftLock;
+                    break;
+
+                default:
+                    EastLock = Lock.SoftLock;
+                    WestLock = Lock.SoftLock;
+                    break;
+            }
+            //same as x but with North/south gates
+            switch(y)
+            {
+                case 0:
+                    NorthLock = Lock.HardLock;
+                    SouthLock = Lock.SoftLock;
+                    break;
+
+                case 4:
+                    NorthLock = Lock.SoftLock;
+                    SouthLock = Lock.HardLock;
+                    break;
+
+                default:
+                    NorthLock = Lock.SoftLock;
+                    SouthLock = Lock.SoftLock;
+                    break;
+            }
+        }
+    }
+    /* Custom lock enum to denote the 3 possible states of a lock
+     * HardLock = Permanent
+     * SoftLock = Potential to unlock
+     * Unlocked = Can traverse through
+     */
+    public enum Lock
+    {
+        HardLock, SoftLock, Unlocked
     }
 }
